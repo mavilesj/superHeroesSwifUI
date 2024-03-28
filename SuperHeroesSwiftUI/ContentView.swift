625c236e4650a10f6b8016c1d6457d1986c8bc91
Miguel Aviles Jenkins <mavilesj@gmail.com>
2024-03-28 05:25:58 -0600

    Initial Commit

---
+++ SuperHeroesSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SuperHeroesSwiftUI
+//
+//  Created by Miguel on 28/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SuperHeroesSwiftUI/SuperHeroesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroesSwiftUIApp.swift
+//  SuperHeroesSwiftUI
+//
+//  Created by Miguel on 28/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
